7934fb95d4e0fd44c4f1de7ebe1ea9b4df55f66d
RaulSanchezwk <raul.sa.work@gmail.com>
2025-05-05 16:05:01 -0600

    Se implementa Layered Architecture

---
+++ app/infrastructure/whatsapp_client.py
@@ -0,0 +1,21 @@
+import httpx
+from app.config import settings
+
+async def send_whatsapp_text_message(recipient_id: str, message: str):
+    url = f"https://graph.facebook.com/v18.0/{settings.PHONE_NUMBER_ID}/messages"
+    headers = {
+        "Authorization": f"Bearer {settings.ACCESS_TOKEN}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "messaging_product": "whatsapp",
+        "to": recipient_id,
+        "type": "text",
+        "text": {
+            "body": message
+        }
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json=payload)
+        print("Respuesta de WhatsApp:", response.status_code, response.json())
+        return response.json()
