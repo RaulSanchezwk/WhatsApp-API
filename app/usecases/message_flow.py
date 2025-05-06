893cb36fb8081187f65a353faf172c3a596087f5
RaulSanchezwk <raul.sa.work@gmail.com>
2025-05-06 11:52:50 -0600

    Lista de números para pruebas

--- app/usecases/message_flow.py
+++ app/usecases/message_flow.py
@@ -3,7 +3,8 @@ from app.infrastructure.database import verificar_si_es_cliente_nuevo
 from app.infrastructure.whatsapp_client import send_whatsapp_text_message
 
 async def manejar_mensaje(cliente: Cliente, mensaje: str, conn):
-    if cliente.telefono == '5218135745910':
+    numeros_permitidos = ['5218135745910', '5218123302217', '5218144883499', '5218116965030', '5218129133326', '5218119043177', '5218182803998', '5218110444217', '5218131240968', '5218182808236']
+    if cliente.telefono in numeros_permitidos:
         respuesta = f"Hola {cliente.nombre}, bienvenido"
         await send_whatsapp_text_message(cliente.telefono, respuesta)
 
